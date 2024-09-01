8d4fb493867e9926bcd2d0a2cf6d507373ce08c5
Nitin Kumar <115390284+Nitinvincible@users.noreply.github.com>
2024-09-01 15:01:56 +0000

    added navigation logo footer and people jpg

--- app.py
+++ app.py
@@ -1,35 +1,38 @@
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
 JOBS = [{
     'id': 1,
     'title': 'Data Analyst',
     'location': 'Bengaluru, India',
-    'Salary': 'Rs. 10,00,000'
+    'salary': 'Rs. 10,00,000'
 }, {
     'id': 2,
     'title': 'Data Scientist',
     'location': 'Delhi, India',
-    'Salary': 'Rs. 15,00,000'
+    'salary': 'Rs. 15,00,000'
 }, {
     'id': 3,
     'title': 'Frontend Engineer',
-    'location': 'Remote',
-    'Salary': 'Rs. 12,00,000'
+    'location': 'Remote'
 }, {
     'id': 4,
     'title': 'Backend Engineer',
     'location': 'San Francisco, USA',
-    'Salary': '$120,000'
+    'salary': '$150,000'
 }]
 
 
 @app.route("/")
-def hello():
-  return render_template("home.html", jobs=JOBS, company_name="Nitian")
+def hello_jovian():
+    return render_template('home.html', jobs=JOBS, company_name='Jovian')
+
 
 @app.route("/api/jobs")
 def list_jobs():
     return jsonify(JOBS)
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
