ea244ca9ad668234ad231540f9a52c2e6a4ba292
Nitin Kumar <115390284+Nitinvincible@users.noreply.github.com>
2024-09-01 16:42:09 +0000

    changed professions for fun

--- app.py
+++ app.py
@@ -4,29 +4,29 @@ app = Flask(__name__)
 
 JOBS = [{
     'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
+    'title': 'Witch',
+    'location': 'Bengaluru, Laal Pahadi, India ',
     'salary': 'Rs. 10,00,000'
 }, {
     'id': 2,
-    'title': 'Data Scientist',
-    'location': 'Delhi, India',
+    'title': 'White Devil',
+    'location': 'Delhi, kali Minar, India',
     'salary': 'Rs. 15,00,000'
 }, {
     'id': 3,
-    'title': 'Frontend Engineer',
+    'title': 'Sarkata',
     'location': 'Remote'
 }, {
     'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
+    'title': 'Munjya',
+    'location': 'Jamna par, Delhi, India',
     'salary': '$150,000'
 }]
 
 
 @app.route("/")
 def hello_Nitian():
-    return render_template('home.html', jobs=JOBS, company_name='Nitian')
+    return render_template('home.html', jobs=JOBS, company_name='Abnormal Recruiters')
 
 
 @app.route("/api/jobs")
