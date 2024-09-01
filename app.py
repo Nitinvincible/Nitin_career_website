7ca0e51ff3d8bc393ce07b1377ba0c1cbd6fcb77
Nitin Kumar <115390284+Nitinvincible@users.noreply.github.com>
2024-09-01 17:49:10 +0000

    added nagin

--- app.py
+++ app.py
@@ -6,27 +6,35 @@ JOBS = [{
     'id': 1,
     'title': 'Witch',
     'location': 'Bengaluru, Laal Pahadi, India ',
-    'salary': 'Rs. 10,00,000'
+    'salary': 'Rs. 5,00,00,000'
 }, {
     'id': 2,
     'title': 'White Devil',
     'location': 'Delhi, kali Minar, India',
-    'salary': 'Rs. 15,00,000'
+    'salary': 'Rs. 3,50,00,000'
 }, {
     'id': 3,
     'title': 'Sarkata',
-    'location': 'Remote'
+    'location': 'Remote',
+    'salary': 'Rs. 4,00,00,000'
 }, {
     'id': 4,
     'title': 'Munjya',
     'location': 'Jamna par, Delhi, India',
-    'salary': '$150,000'
+    'salary': '$6,00,50,000'
+}, {
+    'id': 5,
+    'title': 'Nagin',
+    'location': 'Jamna par, Delhi, India',
+    'salary': '$12,00,50,000'
 }]
 
 
 @app.route("/")
 def hello_Nitian():
-    return render_template('home.html', jobs=JOBS, company_name='Abnormal Recruiters')
+    return render_template('home.html',
+                           jobs=JOBS,
+                           company_name='Abnormal Recruiters')
 
 
 @app.route("/api/jobs")
